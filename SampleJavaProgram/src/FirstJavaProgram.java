82d7f19862704d479daa95d6409cba359b7e72d1
SwethaChavalla <schavalla@hrgdev.com>
2017-05-03 16:30:28 +0400

    Change in output

--- SampleJavaProgram/src/FirstJavaProgram.java
+++ SampleJavaProgram/src/FirstJavaProgram.java
@@ -12,7 +12,7 @@ public class FirstJavaProgram {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hellow Workspace1");
+		System.out.println("Swetha shopia");
 
 	}
 
